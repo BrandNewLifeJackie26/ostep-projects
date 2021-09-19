cadfdda75c00c6c86a27323f8182eb155712deae
BrandNewLifeJackie26 <jackie020130923@126.com>
2021-09-18 23:59:05 -0700

    shell: basic cli features

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -1,5 +1,8 @@
 #include "wish.h"
 
+char *paths[MAX_PATH_COUNT];
+int path_count = 0;
+
 // Error message
 void __error() {
     char error_message[30] = "An error has occurred\n";
@@ -15,9 +18,16 @@ void __exit() {
 
 /*
  * Built-in cd method: change directory
+ * param: 
+ *  path: directory to change to
+ * 
+ * ref: chdir()
+ * 
+ * e.g.
+ * cd /bin
  */
-void __cd(const char *arg) {
-    // ref: chdir
+void __cd(const char *path) {
+    if (chdir(path) == -1) __error();
 }
 
 /*
@@ -25,28 +35,99 @@ void __cd(const char *arg) {
  * It will overwrite the old path with the newly specified path.
  * 
  * e.g.
- * path /bin /usr/bin
+ * path /bin /usr/bin -> resulting paths ['/bin', '/usr/bin']
+ * path -> resulting paths []
  */
-void __path(void *argv[]) {
+void __path(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        paths[i] = argv[i];
+    }
 
+    // All paths after argc will be omitted
+    path_count = argc;
 }
 
 /*
- * Execute the command
+ * Execute the command.
+ * 
+ * ostream: output stream (stdout or a specific file)
+ * argc: argument number
+ * argv[0]: executable or built-in
+ * argv[1] ~ argv[argc-1]: arguments
+ * 
  * // TODO: support absolute and relative paths
  */
-int execute_command(void *args) {
-    // Find executables in pre-defined paths
-    // ref: access()
-    return 0;
-}
-
-/*
- * Display output to output stream
- */
-int display_output(char *output, FILE *file) {
+int execute_command(FILE *ostream, int argc, char *argv[]) {
     int rc = 0;
-    rc = fwrite(output, sizeof(char), strlen(output), file);
+
+    if (argc < 1) {
+        __error();
+        return -1;
+    }
+    char *command_name = argv[0];
+
+    // Built-in methods
+    if (strcmp(command_name, EXIT_COMMAND) == 0) {
+        if (argc > 1) __error();
+        else __exit();
+    }        
+    else if (strcmp(command_name, CD_COMMAND) == 0) {
+        if (argc > 2) __error();
+        else __cd(argv[1]);
+    } else if (strcmp(command_name, PATH_COMMAND) == 0) {
+        __path(argc-1, argv+1);
+    }
+
+    // Executables
+    else {
+        char executable[MAX_EXECUTABLE_COUNT];
+        int pid = 0;
+        int executable_found = 0;
+        for (int i = 0; i < path_count; i++) {
+            strcpy(executable, paths[i]);
+            strcat(executable, "/");
+            strcat(executable, command_name);
+
+            // Execute the accessible executable
+            // Find executables in pre-defined paths
+            // ref: access()
+            if (access(executable, X_OK) == 0) {
+                executable_found = 1;
+                pid = fork();
+
+                // fork() fails
+                if (pid == -1) {
+                    __error();
+                }
+                
+                // execv() in child process
+                else if (pid == 0) {
+                    // BUG: ls . -> cannot access ''?
+                    if (execv(executable, argv) == -1) {
+                        __error();
+                        exit(1);
+                    }
+                } else {
+                    // parent process will wait until child finishes
+                    if (waitpid(pid, NULL, 0) == -1) {
+                        __error();
+                    }
+                }
+                break;
+            }
+        }
+
+        // Cannot find the executable
+        if (executable_found == 0) __error();
+    }
+    
+    // Display output if needed
+    // Redirect by overwriting stdout
+    // for (int i = 0; i < argc; i++) {
+    //     rc = fwrite(argv[i], sizeof(char), strlen(argv[i]), ostream);
+    //     char enter = '\n';
+    //     rc = fwrite(&enter, sizeof(char), 1, ostream);
+    // }
     return rc;
 }
 
@@ -55,13 +136,19 @@ int display_output(char *output, FILE *file) {
  */
 int execute_commands(FILE *file) {
     char *line = NULL;
-    char *output = NULL;
     size_t len = 0;
     int nread = 0;
+    int rc = 0;
     FILE *ostream = NULL;
+
+    // Command arguments init
+    char *arg = NULL;
+    char **argv = NULL;
+    int argc = 0;
+
     while (1) {
         // Prompt
-        printf("%s", PROMPT);
+        if (file == stdin) printf("%s", PROMPT);
 
         // Get the line
         // ref: getline()
@@ -73,7 +160,19 @@ int execute_commands(FILE *file) {
         if (nread == -1) break;
 
         // Parse the line
+        // TODO: redirect
         // ref: strsep()
+        arg = line;
+        argc = 0;
+        argv = (char **)malloc(MAX_ARGC * sizeof(char *));
+        if (argv == NULL) {
+            __error();
+            exit(1);
+        }
+        while ((arg = strsep(&line, DELIMETER)) != NULL && *arg != '\0') {
+            argv[argc] = arg;
+            argc++;
+        }
 
         // Execute command in new processes (including parallel situation),
         // and output them one by one either to stdout or file
@@ -85,16 +184,25 @@ int execute_commands(FILE *file) {
         //     output = execute_command(argv[i]);
         //     display_output(output, ostream);
         // }
-        output = line;
-        display_output(output, ostream);
+
+        // Single command
+        rc = execute_command(ostream, argc, argv);
+        
+        // Free space
+        free(argv);
     }
-    return 0;
+
+    return rc;
 }
 
 int main(int argc, char const *argv[])
 {
     int rc = 0;
     FILE *istream;
+
+    // Initialize paths
+    paths[0] = "/bin";
+    path_count = 1;
 
     // Iteractive mode
     // File stream is stdin
--- processes-shell/wish.h
+++ processes-shell/wish.h
@@ -3,15 +3,26 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <string.h>
 
-const char* PROMPT = "wish> ";
+#define MAX_ARGC 16
+#define MAX_PATH_COUNT 32
+#define MAX_EXECUTABLE_COUNT 128
+
+const char *PROMPT = "wish> ";
+
+const char *DELIMETER = " \n";
+
+const char *EXIT_COMMAND = "exit";
+const char *CD_COMMAND = "cd";
+const char *PATH_COMMAND = "path";
 
 void __error();
 void __exit();
 void __cd(const char *arg);
-void __path(void *argv[]);
-int execute_command(void *args);
+void __path(int argc, char *argv[]);
+int execute_command(FILE *ostream, int argc, char *argv[]);
 int execute_commands(FILE *file);
 
